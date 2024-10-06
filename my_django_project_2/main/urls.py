03d2179446a93bb93764994a21f3d25c17d822ec
janisfs <janisfs@mail.ru>
2024-10-06 23:13:59 +0300

    add new code

--- my_django_project_2/main/urls.py
+++ my_django_project_2/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', views.index, name='index'),   # главная страница сайта 'home'
+    path('about/', views.about, name='about'),  # страница 'about'
 ]
